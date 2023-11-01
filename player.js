ba9c8950a4f608d1124e5d5af51b9525cf37bea4
HellgrenR <rallenolltva@gmail.com>
2023-11-01 14:13:15 +0100

    Adds player name to players.text

--- player.js
+++ player.js
@@ -1,9 +1,10 @@
-
+import { appendFileSync } from "node:fs"
 
 export default class Player {
 
   constructor(name) {
     this.name = name
+    appendFileSync("players.txt", `Player: ${name}\n`, "utf-8")
   }
 
 }
