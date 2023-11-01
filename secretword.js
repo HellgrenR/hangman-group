34fc95e3b1ec7583884fbe9c78431de85229354e
HellgrenR <rallenolltva@gmail.com>
2023-11-01 18:01:25 +0100

    wordLength and isLetterInWord()

--- secretword.js
+++ secretword.js
@@ -1,13 +1,20 @@
 
 export default class SecretWord{
 
-  #word
+  chars = []
 
   constructor(word) {
-    this.#word = word
+    this.chars = word.toUpperCase().split("")
   }
 
+  get wordLength() {
+  return this.chars.length
+  }  
   
+  isLetterInWord(letter) {
+    return this.chars.includes(letter)
+  }
+
 
 }
 
